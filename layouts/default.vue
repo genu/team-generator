bcb1510cde0ef311f4e70eb31fe4e1512d156685
Eugen Istoc <eugenistoc@gmail.com>
2023-01-10 10:00:10 -0500

    fix layout glitches

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="bg-gray-100 flex flex-col">
-    <div class="z-20 w-full md:w-10/12 md:w-2/3 mx-auto max-w-4xl">
+    <div class="z-20 w-full md:w-2/3 mx-auto max-w-4xl">
       <slot />
     </div>
   </div>
