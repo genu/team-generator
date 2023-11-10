6e127d2c9603b02c9d7285d32f92a6eec91b5726
Eugen Istoc <eugenistoc@gmail.com>
2023-11-10 00:26:53 -0500

    cleanup prisma

--- server/api/team.get.ts
+++ server/api/team.get.ts
@@ -1,8 +1,7 @@
 export default defineEventHandler(async (event) => {
-  const client = usePrisma()
   const query = getQuery(event)
 
-  return await client.league.findUnique({
+  return await $prisma.league.findUnique({
     where: { hash: query.teamHash as string },
   })
 })
--- server/utils/prisma.ts
+++ server/utils/prisma.ts
@@ -2,4 +2,4 @@ import { PrismaClient } from '@prisma/client'
 
 const client = new PrismaClient()
 
-export const usePrisma = () => client
+export const $prisma = client
