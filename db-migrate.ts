2d959c303371369e8ba6361f52fd2690a8013731
Eugen Istoc <eugenistoc@gmail.com>
2023-11-13 11:02:44 -0500

    Refactor database migration code to execute as a transaction

--- db-migrate.ts
+++ db-migrate.ts
@@ -4,8 +4,6 @@ import { map } from 'lodash-es'
 
 const $prisma = new PrismaClient()
 
-const leagues = await $prisma.league.findMany()
-
 const parseAsInt = (value: string | number | undefined) => {
   switch (typeof value) {
     case 'string':
@@ -17,42 +15,46 @@ const parseAsInt = (value: string | number | undefined) => {
   }
 }
 
-leagues.forEach(async (league) => {
-  const data: Data = league.data as unknown as Data
+await $prisma.$transaction(async (prisma) => {
+  const leagues = await $prisma.league.findMany()
 
-  const configuration = {
-    rules: data.config.rules as unknown as Prisma.JsonObject,
-    teamCount: parseAsInt(data.config.teamCount),
-  }
+  leagues.forEach(async (league) => {
+    const data: Data = league.data as unknown as Data
 
-  await $prisma.account.create({
-    data: {
-      hash: league.hash!,
-      leagues: {
-        create: {
-          name: data.config.leagueName!,
-          hash: '--imported--',
-          data: {},
-          configuration,
-          players: {
-            createMany: {
-              data: [
-                ...map<LegacyPlayer, any>(data.players, (player) => ({
-                  name: player.name,
-                  rank: parseAsInt(player.rank),
-                  isActive: player.yes || false,
-                  isGoalie: player.gk || false,
-                })),
-              ],
+    const configuration = {
+      rules: data.config.rules as unknown as Prisma.JsonObject,
+      teamCount: parseAsInt(data.config.teamCount),
+    }
+
+    await $prisma.account.create({
+      data: {
+        hash: league.hash!,
+        leagues: {
+          create: {
+            name: data.config.leagueName!,
+            hash: '--imported--',
+            data: {},
+            configuration,
+            players: {
+              createMany: {
+                data: [
+                  ...map<LegacyPlayer, any>(data.players, (player) => ({
+                    name: player.name,
+                    rank: parseAsInt(player.rank),
+                    isActive: player.yes || false,
+                    isGoalie: player.gk || false,
+                  })),
+                ],
+              },
             },
-          },
-          snapshots: {
-            create: {
-              data: data.snapshot as any,
+            snapshots: {
+              create: {
+                data: data.snapshot as any,
+              },
             },
           },
         },
       },
-    },
+    })
   })
 })
