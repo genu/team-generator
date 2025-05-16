cb3dc83cdc413a79d36ad0e3baf9d4b15f873629
Eugen Istoc <eugenistoc@gmail.com>
2025-05-16 17:04:32 -0400

    refactor: remove unused account and league snapshot API endpoints and composables

--- server/api/account/league/[id]/snapshot/.get.ts
+++
@@ -1,9 +0,0 @@
-export default defineEventHandler(async (event) => {
-  const { leagueId } = getQuery(event)
-
-  return $database().db.snapshot.findMany({
-    where: {
-      leagueId: parseInt(leagueId as string),
-    },
-  })
-})
